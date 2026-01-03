6104f01405d17b245dd6d93849e308c4ebf89f73
Himanshu4210 <149389359+Himanshu4210@users.noreply.github.com>
2026-01-03 18:58:54 +0000

    Container config done

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "himanshu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword = "212121"
+accountCity = "Madrid"
+console.table([accountEmail,accountPassword,accountCity]);
