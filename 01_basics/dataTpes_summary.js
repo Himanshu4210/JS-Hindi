3bf2018aa1479e49615e97dfaab719c067e4d88a
Himanshu4210 <149389359+Himanshu4210@users.noreply.github.com>
2026-01-04 09:21:00 +0000

    dataType summary

---
+++ 01_basics/dataTpes_summary.js
@@ -0,0 +1,31 @@
+// Primitive dataTypes
+
+// 7 types : String , Number , Boolean , Null, Undefined, Symbol, BigInt
+
+
+// Reference type or Non Primitive 
+
+// Arrays , Object, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const OutTemperature = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+const arrays = ["S", "H", "I"];
+
+let obj1 = {
+    name:  "HS",
+    age: 22,
+}
+
+const myFun = function(){
+    console.log("Hello");
+}
