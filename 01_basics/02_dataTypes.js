e658e802c244224aaf1f963f5ec689e8791a4382
Himanshu4210 <149389359+Himanshu4210@users.noreply.github.com>
2026-01-04 07:05:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // We are using node.js , not browser that why error shows
+
+
+console.log(3
+    +
+3) // we should not write code like this because code readability should be high
+
+let name = "Himanshu"
+let age = 20
+let isLoggedIn = true
+let state;
+let city = null
+
+// Primitive dataTypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
