50d63a10b6d969ba7356785bef2f3e4a74801eff
Himanshu4210 <149389359+Himanshu4210@users.noreply.github.com>
2026-01-05 09:27:15 +0000

    Maths done

---
+++ 01_basics/05_Nums_and_Maths.js
@@ -0,0 +1,11 @@
+//Maths 
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,2,3,4));
+//console.log(Math.max(3,4,5,6));
+
+console.log(Math.random());
